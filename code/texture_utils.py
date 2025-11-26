86f74a70998e63d6d41c13a2552f78c7fe0e0772
Sobhan_H99 <108659215+Sobhanh99@users.noreply.github.com>
2025-11-27 00:38:39 +0330

    Update texture_utils.py

--- code/texture_utils.py
+++ code/texture_utils.py
@@ -1,45 +1,43 @@
-import numpy as np 
-import matplotlib.pyplot as plt
+#texture utils
+
 
 def compute_mean_texture(data):
+    N = data.shape[0]
+    H = data.shape[1]
+    W = data.shape[2]
 
-	N = data.shape[0]
-	H = data.shape[1]
-	W = data.shape[2]
+    # Parameters for lighting normalization
+    alpha = np.ones((N, 1))  # Scaling
+    beta = np.zeros((N, 1))  # Offset
 
-	# Parameters for lighting normalization
-	alpha = np.zeros((N,1)) # Scaling 
-	beta = np.zeros((N,1)) # Offset 
+    # initialize one of the images as the mean to begin the optimization process
+    mean_texture = np.expand_dims(data[np.random.randint(N)], 0)
 
-	# initialize one of the images as the mean to begin the optimization process
-	mean_texture = np.expand_dims(data[np.random.randint(N)], 0)
+    prev_texture = mean_texture
 
-	prev_texture = mean_texture
+    counter = 0
+    while True:
+        # Compute the scaling and offset parameters
+        beta = np.mean(data, axis=(1, 2, 3), keepdims=True)
+        alpha = np.sqrt(np.mean((data - beta) ** 2, axis=(1, 2, 3), keepdims=True))
 
-	counter = 0
-	while True:
+        # Normalize the data
+        normalized_data = (data - beta) / alpha
 
-		beta = np.mean(data, axis=(1,2), keepdims=True)
-		alpha = np.mean(data*mean_texture, axis=(1,2), keepdims=True)
+        # Compute the new mean texture
+        mean_texture = np.mean(normalized_data, axis=0, keepdims=True)
 
-		normalized_data = (data-beta)/alpha
+        # Standardize the mean texture
+        mean_texture -= np.mean(mean_texture, axis=(1, 2, 3), keepdims=True)
+        mean_texture /= np.sqrt(np.mean(mean_texture ** 2, axis=(1, 2, 3), keepdims=True))
 
-		mean_texture = np.mean(normalized_data, axis=0, keepdims=True)
-		# Standardize mean
+        if np.linalg.norm(prev_texture - mean_texture) < 0.0000001:
+            break
 
-		mean_texture -= np.mean(mean_texture, axis=(1,2), keepdims=True)
-		mean_texture /= np.mean((mean_texture**2), axis=(1,2), keepdims=True)
-		# 
-		# mean_texture =  (mean_texture-beta)/alpha
+        prev_texture = mean_texture
+        counter += 1
 
-		if np.linalg.norm(prev_texture-mean_texture) < 0.0000001:
-			break
+    print('Texture mean found in {} iterations'.format(counter))
+    normalized_mean = (mean_texture - np.min(mean_texture)) / (np.max(mean_texture) - np.min(mean_texture))
 
-		prev_texture = mean_texture
-		counter += 1
-
-	print('Texture mean found in {} iterations'.format(counter))
-
-	return mean_texture, beta, alpha, normalized_data
-
-
+    return normalized_mean, beta, alpha, normalized_data
